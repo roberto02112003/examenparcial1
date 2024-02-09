e04dfc2d2d8584dcac02c2d93331adaad4a26dec
roberto02112003 <157657915+roberto02112003@users.noreply.github.com>
2024-02-08 20:09:27 -0600

    Update sesion.dart

--- lib/sesion.dart
+++ lib/sesion.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'registro',
+      title: 'INICIO DE SESION EXITOSO',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
